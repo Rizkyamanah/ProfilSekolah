16d351a323c6af0f764991c588cfff05ca6eeba7
Rizkyamanah <januaranafi@gmail.com>
2022-02-17 14:07:14 +0700

    android profil sekolah

--- app/src/main/java/com/example/profilsekolah/fasilitasActivity.java
+++ app/src/main/java/com/example/profilsekolah/fasilitasActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
 public class fasilitasActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+    int[] images = {R.drawable.ekskul1,
+            R.drawable.ekskul2,R.drawable.ekskul1,R.drawable.ekskul2};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fasilitas);
+
+        sliderView = findViewById(R.id.image_slider);
+
+        SliderAdapter sliderAdapter = new SliderAdapter(images);
+
+        sliderView.setSliderAdapter(sliderAdapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
+        sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
+        sliderView.startAutoCycle();
     }
 }
